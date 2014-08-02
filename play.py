85babc009353245779460b94701793e9566209ef
mike stedman <github@ravuya.com>
2014-08-02 17:11:59 -0600

    getting ready to load sample data

--- play.py
+++ play.py
@@ -4,16 +4,17 @@ def read_sample(fp):
   s = Sample()
   s.name = fp.read(22)
   s.length = struct.unpack('>H', fp.read(2))[0]
-  s.finetune = fp.read(1)
-  s.volume = fp.read(1)
+  s.finetune = struct.unpack('B', fp.read(1))[0]
+  s.volume = struct.unpack('B', fp.read(1))[0]
   s.repeat_offset = struct.unpack('>H', fp.read(2))[0]
   s.repeat_length = struct.unpack('>H', fp.read(2))[0]
   return s
 
 def read_format_chunk(song, fp):
-  song.song_positions = fp.read(1)
-  song.restart_position = fp.read(1)
-  song.pattern_data = fp.read(128)
+  song.song_positions = struct.unpack('B', fp.read(1))[0]
+  song.restart_position = struct.unpack('B', fp.read(1))[0]
+  for i in range(0, 128):
+    song.patterns.append(struct.unpack('B', fp.read(1))[0])
   song.format = fp.read(4)
 
 class Song:
@@ -60,11 +61,24 @@ try:
 
   print '# of samples:', len(s.samples)
   for sample in s.samples:
-    print 'sample name:', sample.name
+    print 'sample name:', sample.name, 'length (in words):', sample.length
   print 'Format:', s.format
 
+  # determine the count of patterns
+  pattern_count = max(s.patterns) + 1 # 0-indexed.
+  print 'number of patterns:', pattern_count
+
   # read pattern data
+  for i in range(0, pattern_count):
+    s.pattern_data.append(fp.read(1024)) # TODO: parse
+
   # read sample data
+  for sample_ref in s.samples:
+    if sample_ref.length > 1: # indicating empty sample
+      s.sample_data.append(fp.read(2 * sample_ref.length)) # in words...
+    else:
+      s.sample_data.append([]) # don't break indices!!
+      print 'zero length sample:', sample_ref.name
 
 finally:
   fp.close()
